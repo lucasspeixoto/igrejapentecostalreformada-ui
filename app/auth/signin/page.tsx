73f2cf8189a2e3d4c4b03a00fe7cb99832b35d12
Lucas Peixoto <lspeixotodev@gmail.com>
2023-09-30 21:17:55 -0300

    feat: eslint packages installation and config rules

--- app/auth/signin/page.tsx
+++ app/auth/signin/page.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import Image from "next/image";
 import Breadcrumb from "@/components/Breadcrumbs/Breadcrumb";
 import { Metadata } from "next";
+
 export const metadata: Metadata = {
   title: "Signin Page | Next.js E-commerce Dashboard Template",
   description: "This is Signin page for TailAdmin Next.js",
