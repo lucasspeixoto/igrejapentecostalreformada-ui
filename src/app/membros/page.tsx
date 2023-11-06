818d00b61ae15f812fd6489d26c081afd60914ec
Lucas Peixoto <lspeixotodev@gmail.com>
2023-11-05 22:01:39 -0300

    populate address by request values from via cep api with cep

--- src/app/membros/page.tsx
+++ src/app/membros/page.tsx
@@ -1,16 +1,10 @@
 import type { Metadata } from 'next';
 
-import { useAuthContext } from '@/providers/AuthContextProvider';
-
-import UserDetail from './profile/UserDetail';
-
 export const metadata: Metadata = {
   title: 'Igreja Pentecostal reformada',
   description: 'Cadastro',
 };
 
 export default function Membros() {
-  const useAuth = useAuthContext()!;
-
-  return <>{useAuth.isAuthenticated ? <UserDetail /> : null}</>;
+  return <></>;
 }
