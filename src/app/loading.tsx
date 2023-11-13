d324a2b8567c4b9a7a9b8f9c724c80b5b42d911e
Lucas Peixoto <lspeixotodev@gmail.com>
2023-11-13 08:57:53 -0300

    Get selected user data in UserBaseData

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,6 +1,6 @@
 const Loading = () => {
   return (
-    <div className="flex h-screen items-center justify-center bg-white">
+    <div className="bg-slate-500 z-20 flex h-screen items-center justify-center opacity-65">
       <div className="h-16 w-16 animate-spin rounded-full border-4 border-solid border-primary border-t-transparent"></div>
     </div>
   );
--- src/components/MemberLoading/index.tsx
+++ src/components/MemberLoading/index.tsx
@@ -1,6 +1,6 @@
 const MemberLoading = () => {
   return (
-    <div className="bg-slate-500 z-20 flex h-screen items-center  justify-center opacity-65">
+    <div className="bg-slate-500 z-20 flex h-screen items-center justify-center opacity-65">
       <div className="h-16 w-16 animate-spin rounded-full border-4 border-solid border-primary border-t-transparent"></div>
     </div>
   );
