44cd65aa0e4c88b58e2ad248249c04d5cb68f14b
Lucas Peixoto <lspeixotodev@gmail.com>
2024-01-02 12:18:06 -0300

    Testing update in GH actions yaml

--- src/app/cadastro/page.tsx
+++ src/app/cadastro/page.tsx
@@ -7,7 +7,7 @@ import RedirectLink from '../components/RedirectLink';
 import SignUpForm from './SignUpForm';
 
 export const metadata: Metadata = {
-  title: 'Cadastro',
+  title: 'Cadastrar',
   description: 'Plataforma de membros da igreja pentecostal reformada (IPR)',
 };
 
