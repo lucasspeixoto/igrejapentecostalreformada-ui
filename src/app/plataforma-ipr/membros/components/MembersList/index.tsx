765f498e396394ef0e0335207455022edaeac854
Lucas Peixoto Fernandes <lspeixotodev@gmail.com>
2024-05-01 10:03:20 -0300

    Finish finances v1

--- src/app/plataforma-ipr/membros/components/MembersList/index.tsx
+++ src/app/plataforma-ipr/membros/components/MembersList/index.tsx
@@ -5,20 +5,18 @@
 
 import Loader from '@components/common/Loader';
 import { getUsersDocuments } from '@fire/firestore/getData';
-import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 import React from 'react';
-import { AiOutlineCheckCircle } from 'react-icons/ai';
-import { BiBlock } from 'react-icons/bi';
 
+import useWindowDimensions from '@/hooks/useWindowDimensions';
 import { useAuthContext } from '@/providers/AuthContextProvider';
 import type { UserData } from '@/types/user-data';
-import { orderMembersListByName } from '@/utils/array-operations';
 
-import MemberDeleteAction from './components/MemberDeleteAction';
-import MemberDetailAction from './components/MemberDetailAction';
-import MembersTableColumns from './components/MembersTableColumns';
+import MembersDesktopView from './components/MembersDesktopView';
+import MembersMobileView from './components/MembersMobileView';
 import MembersTableInfo from './components/MembersTableInfo';
+
+const MAX_MOBILE_WIDTH = 768;
 
 const MembersList: React.FC = () => {
   const userContext = useAuthContext();
@@ -30,6 +28,8 @@ const MembersList: React.FC = () => {
   const [isAdminOption, setIsAdminOption] = React.useState(false);
 
   const router = useRouter();
+
+  const isMobileSize = useWindowDimensions(MAX_MOBILE_WIDTH);
 
   React.useEffect(() => {
     let mounted = true;
@@ -66,75 +66,7 @@ const MembersList: React.FC = () => {
             {isLoadingUsers || !userLoadedData.length ? (
               <Loader />
             ) : (
-              <>
-                <div className="rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
-                  <div className="max-w-full overflow-x-auto">
-                    <table className="w-full table-auto">
-                      <MembersTableColumns />
-                      <tbody>
-                        {React.Children.toArray(
-                          userLoadedData?.sort(orderMembersListByName).map(member => (
-                            <tr className="hover:bg-gray-2 hover:dark:bg-meta-4 hover:cursor-pointer">
-                              <td className="border-b border-[#eee] px-0 py-5 pl-9 xl:pl-5 dark:border-strokedark">
-                                <div className="flex flex-row items-center gap-4 font-medium text-black dark:text-white">
-                                  <div className="shrink-0">
-                                    <>
-                                      {member.auth?.photoUrl ? (
-                                        <Image
-                                          src={member.auth?.photoUrl!}
-                                          alt="Foto membro"
-                                          width={32}
-                                          height={32}
-                                          className="size-10 rounded-full"
-                                        />
-                                      ) : (
-                                        <Image
-                                          src={'/images/user/dummy-user.png'}
-                                          alt="Brand"
-                                          width={32}
-                                          height={32}
-                                          className="size-10 rounded-full"
-                                        />
-                                      )}
-                                    </>
-                                  </div>
-                                  <p className="hidden text-black sm:block dark:text-white">
-                                    {member.auth?.name.split(' ')[0]}{' '}
-                                  </p>
-                                </div>
-                              </td>
-                              <td className="border-b border-[#eee] px-4 py-5 dark:border-strokedark">
-                                <p className="text-black dark:text-white">{member?.auth?.role}</p>
-                              </td>
-                              <td className="border-b border-[#eee] px-4 py-5 dark:border-strokedark">
-                                <p className="text-black dark:text-white">
-                                  {member?.auth?.isAdmin ? 'Admin' : 'Membro'}
-                                </p>
-                              </td>
-                              <td className="border-b border-[#eee] px-4 py-5 dark:border-strokedark">
-                                <p className="text-black dark:text-white">
-                                  {member?.process?.isRegistered ? (
-                                    <AiOutlineCheckCircle size={20} className="text-meta-3" />
-                                  ) : (
-                                    <BiBlock size={20} className="text-meta-7" />
-                                  )}
-                                </p>
-                              </td>
-                              <td className="border-b border-[#eee] px-4 py-5 dark:border-strokedark">
-                                <div className="flex items-center space-x-3.5">
-                                  <MemberDetailAction userId={member?.auth.userId} />
-
-                                  <MemberDeleteAction userId={member?.auth.userId} />
-                                </div>
-                              </td>
-                            </tr>
-                          ))
-                        )}
-                      </tbody>
-                    </table>
-                  </div>
-                </div>
-              </>
+              <>{isMobileSize ? <MembersMobileView members={userLoadedData} /> : <MembersDesktopView members={userLoadedData} />}</>
             )}
           </div>
         </>
